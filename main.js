a43d58247d5bf607087ae714aab2b0393e30668b
jacopo-derrico <derricoj.design@gmail.com>
2024-01-29 17:09:28 +0100

    input pushes new li into array

--- main.js
+++ main.js
@@ -3,11 +3,17 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
-            
+            newTask: '',
+            tasks: [],
         }
     },
     methods: {
-        
+        addTask() {
+            if (this.newTask !== '') {
+                this.tasks.push({text: this.newTask, done: false})
+            }
+            this.newTask = ''
+        },
     },
     
 }).mount('#app')
